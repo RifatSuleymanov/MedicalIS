ec733b51670432b3f3154c0a3632e4a3664b9343
conor <fc.chelsea11@yandex.ru>
2024-05-12 20:35:27 +0300

    start project

---
+++ src/main/java/ru/rkod/rkod/RkodApplication.java
@@ -0,0 +1,13 @@
+package ru.rkod.rkod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RkodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rkod/rkod/RkodApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rkod.rkod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RkodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
