d4155e025233ee1abbdbc9c8ea5de4278e0c13dd
RIfatSuleymanov <fc.chelsea11@yandex.ru>
2024-10-17 10:11:11 +0300

    add EmployeeServiceImpTest.java

--- src/main/java/ru/rkod/rkod/dto/EmployeeDto.java
+++ src/main/java/ru/rkod/rkod/dto/EmployeeDto.java
@@ -10,12 +10,12 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor(force = true)
 public class EmployeeDto extends Employee {
-    private Integer id; //id
-    public String name; // Имя
-    public String lastName; // Фамилия
-    private String middleName; // Отчество
-    private String position; // Должность
-    private Department department; // Отделение
-    private String email; // email
-    private LocalDate dateOfEmployment; // Дата приемна на работу
+    private Integer id;
+    public String name;
+    public String lastName;
+    private String middleName;
+    private String position;
+    private Department department;
+    private String email;
+    private LocalDate dateOfEmployment;
 }
