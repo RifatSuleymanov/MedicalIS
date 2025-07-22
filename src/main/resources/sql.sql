9e0d9dd5f0212222892a081396f99132308f386d
user <fc.chelsea11@yandex.ru>
2025-07-22 11:24:03 +0300

    sql posgresql

--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -1,34 +1,39 @@
-DROP TABLE IF EXISTS roles, employee, department, patient;
+-- Удаление таблиц, если они существуют
+DROP TABLE IF EXISTS employee_roles, patient, employee, department, roles;
+
+-- Создание таблицы ролей
 CREATE TABLE roles (
-                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       id SERIAL PRIMARY KEY,
                        role_name VARCHAR(255) NOT NULL UNIQUE,
                        role_description VARCHAR(255)
 );
 
+-- Создание таблицы сотрудников
 CREATE TABLE employee (
-                          id INT AUTO_INCREMENT PRIMARY KEY,
+                          id SERIAL PRIMARY KEY,
                           name VARCHAR(30) NOT NULL,
                           lastname VARCHAR(30) NOT NULL,
                           patronymic VARCHAR(255),
                           position VARCHAR(255) NOT NULL,
                           email VARCHAR(255) NOT NULL,
-                          dateOfEmployment DATE
+                          dateOfEmployment DATE,
+                          department_id INT  -- Добавлено сразу сюда для компактности
 );
 
+-- Создание таблицы отделов
 CREATE TABLE department (
-                            id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                            id SERIAL PRIMARY KEY,
                             department_name VARCHAR(255) NOT NULL UNIQUE,
                             department_description VARCHAR(255) NOT NULL,
                             department_manager INT,
                             CONSTRAINT fk_department_manager FOREIGN KEY (department_manager) REFERENCES employee(id)
 );
 
-ALTER TABLE employee
-    ADD COLUMN department_id BIGINT;
-
+-- Добавление внешнего ключа для связи сотрудника с отделом
 ALTER TABLE employee
     ADD CONSTRAINT fk_employee_department FOREIGN KEY (department_id) REFERENCES department(id);
 
+-- Таблица связи между сотрудниками и ролями
 CREATE TABLE employee_roles (
                                 employee_id INT NOT NULL,
                                 role_id INT NOT NULL,
@@ -37,8 +42,9 @@ CREATE TABLE employee_roles (
                                 CONSTRAINT fk_emp_role_role FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
+-- Таблица пациентов
 CREATE TABLE patient (
-                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         id SERIAL PRIMARY KEY,
                          namePatient VARCHAR(30) NOT NULL,
                          lastnamePatient VARCHAR(30) NOT NULL,
                          middleNamePatient VARCHAR(255),
